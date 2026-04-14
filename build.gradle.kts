b76bb9e86d7001f121deb2f9a0aaac5f62e2ff67
Miura-KR <ovis_w6v6_grappler95@yahoo.co.jp>
2026-04-14 20:38:14 +0900

    v1.1.0リリース

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.k.pmpstudy"
-version = "1.0.0"
+version = "1.1.0"
 
 // Set the JVM language level used to build the project.
 kotlin {
@@ -38,9 +38,18 @@ intellijPlatform {
         }
 
         changeNotes = """
-            v1.0.0<br>
-            Initial version
+            <h3>${project.version}</h3>
+            <ul>
+              <li>Added support for moved / renamed files: right-clicking a moved entry now shows
+                  <b>Reveal Pre-Move Location</b>, which focuses the directory the file was in
+                  before the move.</li>
+              <li>The action is now also available in the <b>Git</b> tool window's per-commit
+                  changed files list (in addition to the Commit tool window).</li>
+            </ul>
         """.trimIndent()
+    }
+    publishing {
+        token = providers.gradleProperty("JETBRAIN_TOKEN")
     }
 }
 
